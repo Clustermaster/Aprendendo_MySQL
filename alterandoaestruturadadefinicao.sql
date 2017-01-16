23df474173310ab65de4ae2732795ff2c08e5f69
LucasRR94 <lucasregodarocha@outlook.com>
2017-01-16 18:47:39 -0200

    novos comandos

--- alterandoaestruturadadefinicao.sql
+++ alterandoaestruturadadefinicao.sql
@@ -17,7 +17,43 @@
 				/*nome_velho nome_novo*/
 
 	/*Renomenando a tabela*/
-	use cadastro;
+	/*use cadastro;
 	alter table pessoas
 	rename to gafanhotos;
+	*/
+	/* IF NOT EXISTE cria somente se não existe*/
+	/*
+	USE cadastro;
+	CREATE TABLE IF NOT EXISTS cursos(
+		nome varchar(30) NOT NULL UNIQUE, 
+		descrição text,
+		carga int UNSIGNED,
+		totalaulas int,
+		ano year DEFAULT '2016'
+	)DEFAULT CHARSET = utf8;
+	*/
+	/*adicionando id*/
+	/*
+	ALTER TABLE cursos
+	add column idcurso int first;
+	*/
+	/*
+	ALTER TABLE cursos
+	add primary key (idcurso);
+	describe cursos;
+	*/
+	/*
+	CREATE TABLE if not exists teste(
+		id int,
+		nome varchar(10),
+		idade int,
+
+	);
+
+	insert into teste value
+	('1','Pedro','22'),
+	('2','Maria','12'),
+	('3','Maricota','77');
+
+	*/
 	
