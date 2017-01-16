15e171471ce6286367ed031754985c7f19243ffa
LucasRR94 <lucasregodarocha@outlook.com>
2017-01-16 15:25:38 -0200

    Nova forma de insercao

--- InserindoNaTabela.sql
+++ InserindoNaTabela.sql
@@ -1,4 +1,10 @@
 	USE cadastro;
+	INSERT INTO pessoas 
+	(id, nome , nascimento, sexo, peso, altura, nacionalidade)
+	VALUES
+	(DEFAULT,'Franco','1960-01-30','M','90.5','1.98',DEFAULT),
+	(DEFAULT,'ANA','1980-01-30','F','60.5','1.53',DEFAULT);
 
-	(id,nome, nascimento, sexo , peso, altura, nacionalidade)
-	('1','Godofredo','1984-01-02','M','78.5','1.83','Brasil');
+	INSERT INTO pessoas 
+	VALUES (DEFAULT,'James','1980-01-30','M','70.5','1.68',DEFAULT);
+	
