aba58e346ba7ce0e81486e2d29d8185ed88712c8
LucasRR94 <lucasregodarocha@outlook.com>
2017-01-18 16:25:04 -0200

    novo import

--- Comorealizarbackup.sql
+++ Comorealizarbackup.sql
@@ -1,8 +1,7 @@
-	mysqldump -uroot -p --set-gtid-purged=OFF nomedobancodedados > backupdessebancodedados.sql
+	mysqldump -u root -p --set-gtid-purged=OFF nomedobancodedados > backupdessebancodedados.sql
 	more backupdessebancodedados.sql
 
-	/* importando banco*/
-	mysql -u root -p novobanco < aexportar.sql
+	/* importando banco*/source aexportar.sql
 
 
 	/*novo banco precisa ja existir*/
