31517ae9c344bb0d95f1eb37ace0856396ca3b50
LucasRR94 <lucasregodarocha@outlook.com>
2017-01-28 15:28:37 -0200

    novos comandos

--- chaveestrangeira.sql
+++ chaveestrangeira.sql
@@ -33,3 +33,27 @@
 	select g.nome , c.nome , c.ano	from gafanhotos g right outer join cursos as c 
 	on c.idcurso = g.cursopreferido;
 	/*right e left são usados pra dar prioridade a uma das tabelas*/
+	/*usando duas chaves estrangeiras em um banco de dados*/
+		/*
+	/*relacionamento muito pra muitos
+	use cadastro;
+	create table gafanhoto_assiste_curso(
+		id int not null auto_increment,
+		data date,
+		idgafanhoto int , 
+		idcurso int,
+		primary key (id),
+		foreign key(idgafanhoto) references gafanhotos(id),
+		foreign key (idcurso) references cursos(idcurso)
+	) default charset = utf8;
+
+	insert into gafanhoto_assiste_curso values 
+		(default , "2014-03-01", '1', '2');
+	select * from gafanhoto_assiste_curso; 
+*/
+	select g.nome,c.nome from gafanhotos g
+	join gafanhoto_assiste_curso a 
+	on g.id = a.idgafanhoto 
+	join cursos c 
+	on a.idcurso = c.idcurso
+	order by g.nome;
